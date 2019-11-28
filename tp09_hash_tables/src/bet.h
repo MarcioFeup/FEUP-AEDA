56096ad4501100db49591dc2d736216f2e8f8892
ctrlMarzio <up201909936@fe.up.pt>
2019-11-28 00:13:34 +0000

    [tp09] Finished implementation

--- tp09_hash_tables/src/bet.h
+++ tp09_hash_tables/src/bet.h
@@ -7,19 +7,51 @@
 
 typedef std::unordered_set<unsigned> betting_slip;
 
+/**
+ * Represents a bet. A bet contains a hash table in the form of unordered_set of unsigned, this is, the numbers of the bet.
+ */
 class Bet {
   public:
+	/**
+	 * Constructs a new empty bet.
+	 */
 	Bet();
 
+	/**
+	 * Fulfills the bet with a given amount of numbers from a vector.
+	 *
+	 * @param values the vector of numbers
+	 * @param qt the amount of numbers to add
+	 */
 	void build(const std::vector<unsigned> &values, unsigned qt = 6);
 
+	/**
+	 * Verifies if the vet has a given number.
+	 *
+	 * @param num the given number
+	 * @return true if the bet has the number, false otherwise
+	 */
 	bool has(unsigned num) const;
 
+	/**
+	 * Gets the amount of correct numbers, given a draw.
+	 *
+	 * @param draw the draw
+	 * @return the amount of correct numbers
+	 */
 	unsigned getCorrects(const betting_slip &draw) const;
 
+	/**
+	 * Gets the numbers of the bet.
+	 *
+	 * @return the number
+	 */
 	const betting_slip &getNumbers() const;
 
   private:
+	/**
+	 * The numbers of the bet in an unsigned_set
+	 */
 	betting_slip numbers;
 };
 
--- tp09_hash_tables/src/player.h
+++ tp09_hash_tables/src/player.h
@@ -6,21 +6,58 @@
 
 typedef std::unordered_set<Bet, bet_hash, bet_equals> betting_set;
 
+/**
+ * Represents an actor that has bets.
+ */
 class Player {
   public:
+	/**
+	 * The constructor of the player that initializes it with no bets.
+	 *
+	 * @param name the name of the player
+	 */
 	explicit Player(std::string name = "anonymous");
 
+	/**
+	 * Adds a bet.
+	 *
+	 * @param bet the bet
+	 */
 	void addBet(const Bet &bet);
 
+	/**
+	 * Gets the amount of bets of the player that have a given number
+	 *
+	 * @param number the given number
+	 * @return the amount of bets
+	 */
 	unsigned qtBetsOn(unsigned number) const;
 
+	/**
+	 * Gets the winner bets given a draw.
+	 * A winner bet is a bet that has more than a minimum required correct numbers.
+	 *
+	 * @param draw the draw
+	 * @return a set with the bets
+	 */
 	betting_set getPrizedBets(const betting_slip &draw) const;
 
+	/**
+	 * Gets the amount of bets of the player.
+	 *
+	 * @return the amount of bets
+	 */
 	unsigned getQtBets() const;
 
   private:
+	/**
+	 * The name of the player.
+	 */
 	std::string name;
 
+	/**
+	 * The bets of the player stored in an unordered_set.
+	 */
 	betting_set bets;
 };
 
