0c2a47a5599b267afc2dda078613d092f63c47da
Márcio Duarte <up201909936@fe.up.pt>
2019-12-16 11:46:08 +0000

    [tp11] Finished documentation

--- tp11_disjoint_sets/src/disjointSets.h
+++ tp11_disjoint_sets/src/disjointSets.h
@@ -5,21 +5,62 @@
 
 using namespace std;
 
+/**
+ * Represents a collection of disjoint sets of ints.
+ */
 class DisjointSets {
   public:
+	/**
+	 * Constructs a collection by default.
+	 */
 	DisjointSets();
 
+	/**
+	 * Constructs a collection with a given number of elements.
+	 *
+	 * @param n the number of elements
+	 */
 	explicit DisjointSets(int n);
 
+	/**
+	 * Finds the root of the set in which a given element belongs.
+	 *
+	 * @param v the given element
+	 * @return the root of the set of the given element
+	 */
 	int find(int v) const;
 
+	/**
+	 * Unites two sets given their root.
+	 * The union is made connecting the second root with the first one, this is, the root of the second set will be
+	 * assigned to the root of the first set.
+	 *
+	 * E.g.: [-1, 2, -1, -1, 3]
+	 *         0  1  2   3  4
+	 * unionSets(3, 2), then
+	 *       [-1, 2, -1, 2, 3]
+	 *
+	 * @param root1 the root of the fist set
+	 * @param root2 the root of the second set
+	 */
 	void unionSets(int root1, int root2);
 
+	/**
+	 * Returns the number of sets in the collection, this is the number of cells that are connected.
+	 *
+	 * @return the number of sets
+	 */
 	int getNumberOfSets() const;
 
   private:
+	/**
+	 * The int that indicates that a node is the root of the set.
+	 */
 	static const int EMPTY_SET;
 
+	/**
+	 * The vector that stores the sets.
+	 */
 	vector<int> set;
 };
 
--- tp11_disjoint_sets/src/maze.h
+++ tp11_disjoint_sets/src/maze.h
@@ -3,25 +3,66 @@
 
 #include "disjointSets.h"
 
+/**
+ * Represents a maze.
+ * A maze is complete whenever there exists a path that can go through all the cells in the maze, this is, all the cells
+ * are in the same set.
+ */
 class Maze {
   public:
+	/**
+	 * The constructor of the maze.
+	 * This create a vector where every element (cell) is the root of a set.
+	 *
+	 * @param rows the number of rows of the maze, only for drawing purposes
+	 * @param cols the number of columns of the maze, only for drawing purposes
+	 */
 	Maze(int rows, int cols);
 
+	/**
+	 * Builds a random, complete, maze.
+	 */
 	void buildRandomMaze();
 
+	/**
+	 * Returns the disjoint sets.
+	 *
+	 * @return the disjoint sets
+	 */
 	DisjointSets getSets() const;
 
+	/**
+	 * Prints the maze in the terminal.
+	 */
 	void printMaze() const;
 
   private:
+	/**
+	 * The disjoint sets that implement the maze.
+	 */
 	DisjointSets maze;
 
+	/**
+	 * The number of rows of the maze.
+	 */
 	int nrows = 0;
 
+	/**
+	 * The number of columns of the maze.
+	 */
 	int ncols = 0;
 
+	/**
+	 * A vector that stores the pair of neighbour cells that have no walls between them, this is, that are connected.
+	 */
 	vector<pair<int, int>> no_walls;
 
+	/**
+	 * Gets the neighbour cells of a given cell.
+	 *
+	 * @param x the given cell
+	 * @return the vector with the neighbour cell
+	 */
 	vector<int> getNeighbours(int x) const;
 };
 
